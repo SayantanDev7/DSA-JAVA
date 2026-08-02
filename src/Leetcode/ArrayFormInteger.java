508bc95365dd01f2cd53d41307b8c032f2985d93
Sayantan699 <sayantanhalder43@gmail.com>
2026-08-02 18:03:35 +0530

    Added commit messaages

--- src/Leetcode/ArrayFormInteger.java
+++ src/Leetcode/ArrayFormInteger.java
@@ -8,7 +8,7 @@ public class ArrayFormInteger {
         int[] num = {1, 2,1, 0};
         int k = 34;
 
-        ArrayList<Integer> ans = addToArrayForm(num, k);
+        ArrayList<Integer> ans = addToArrayForm(num, k); //Auxillary space taking SC: O(N)
 
         System.out.println(ans);
     }
@@ -35,8 +35,19 @@ public class ArrayFormInteger {
             k /= 10;
         }
 
-        Collections.reverse(ans);
+        Collections.reverse(ans); //Reverse = O(n)
 
         return ans;
     }
 }
+
+//The loop runs until you've processed
+//
+//every digit of num
+//every digit of k
+//        so number of iterations :-
+//max(
+//        num.length,
+//        number of digits in k
+//)
+//    TC is O(max(n,digits(k)))
