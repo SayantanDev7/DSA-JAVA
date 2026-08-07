827e0b63aa59aabe5c5d69d8a49acc9f12d6115a
Sayantan699 <sayantanhalder43@gmail.com>
2026-08-07 22:13:14 +0530

    first commit

---
+++ src/Stack/MinStack.java
@@ -0,0 +1,39 @@
+package Stack;
+
+import java.util.Stack;
+
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int value) {
+        stack.push(value);
+
+        if(minStack.isEmpty())
+            minStack.push(value);
+        else{
+            int currentmin = Math.min(value,minStack.peek());
+            minStack.push(currentmin);
+        }
+        // minStack.push(value)
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        //we need to solve this in O(1) TC
+        //if we traverse the stack and use Math.min then in WC its O(n)
+        return minStack.peek();
+    }
+}
