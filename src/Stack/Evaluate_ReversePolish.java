5a1b4d03ec55e63017a3f295806d93b7c7d896b4
Sayantan699 <sayantanhalder43@gmail.com>
2026-08-09 21:20:30 +0530

    final code of the leetcode 150 prob

---
+++ src/Stack/Evaluate_ReversePolish.java
@@ -0,0 +1,40 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Evaluate_ReversePolish {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+
+        for(String ele: tokens){
+            if(ele.equals("+") || ele.equals("/") || ele.equals("*") || ele.equals("-")){
+                int opnd2 = st.pop();
+                int opnd1 = st.pop();
+                int res = 0;
+                switch(ele){
+                    case "+" :
+                        res = opnd1 + opnd2;
+                        break;
+                    case "*" :
+                        res = opnd1 * opnd2;
+                        break;
+                    case "-" :
+                        res = opnd1 - opnd2;
+                        break;
+                    case "/" :
+                        res = opnd1 / opnd2;
+                        break;
+
+                }
+                st.push(res);
+            }
+            else{
+                int num = Integer.parseInt(ele);
+                st.push(num);
+
+            }
+        }
+
+        return st.peek();
+    }
+}
