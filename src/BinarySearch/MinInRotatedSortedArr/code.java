d5dc3ad36b7f72cdaae16190fee520232d0d6cee
Sayantan699 <sayantanhalder43@gmail.com>
2026-08-15 23:18:47 +0530

    optmzed approach of Leetcode 153

--- src/BinarySearch/MinInRotatedSortedArr/code.java
+++ src/BinarySearch/MinInRotatedSortedArr/code.java
@@ -1,4 +1,27 @@
 package BinarySearch.MinInRotatedSortedArr;
 
 public class code {
+
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > nums[right]){ // i.e right half is rotated and left half is sorted
+                //since min is on the right half i.e go to right half
+                left = mid + 1;
+            }
+            else{
+                //right half is sorted and left half is rotated
+                //since min is on the left half it can be mid itself also
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
 }
+
+// // You never searched through the elements.
+
+/// / You simply kept narrowing the possible location of the minimum
